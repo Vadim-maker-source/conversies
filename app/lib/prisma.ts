a8f048ccc5226adc542301d2b59df2417e45e0b6
Vadim <Vadimbureev380@gmail.com>
2025-11-17 20:20:13 +0300

    16

--- app/lib/prisma.ts
+++ app/lib/prisma.ts
@@ -1,23 +1,11 @@
 import { PrismaClient } from "../generated/prisma/client";
 
-// Workaround для Vercel
-function createPrismaClient() {
-  if (process.env.NODE_ENV === 'production') {
-    return new PrismaClient({
-      log: ['error'],
-      datasourceUrl: process.env.DATABASE_URL,
-    });
-  } else {
-    return new PrismaClient({
-      log: ['query', 'error', 'warn'],
-    });
-  }
-}
-
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
-export const prisma = globalForPrisma.prisma || createPrismaClient();
+export const prisma =
+  globalForPrisma.prisma ||
+  new PrismaClient({
+    log: ["error", "warn"],
+  });
 
-if (process.env.NODE_ENV !== 'production') {
-  globalForPrisma.prisma = prisma;
-}
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
